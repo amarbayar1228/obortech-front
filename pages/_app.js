b8e8154d268214cf49f843f39ade53e463995a29
PVZ 2 <amarbayarbatbayar2@gmail.com>
2022-10-14 17:22:11 +0800

    a

--- pages/_app.js
+++ pages/_app.js
@@ -15,33 +15,31 @@ function MyApp({ Component, pageProps }) {
     const router = useRouter(); 
     const [loading, setLoading] = useState(false); 
  
-    useEffect(() => { 
-        // page loading..
-        const handleStart = (url) => (url !== router.asPath) && setLoading(true);
-        const handleComplete = (url) => (url === router.asPath) && setTimeout(() =>{setLoading(false)},5000);
+useEffect(() => { 
+// page loading..
+const handleStart = (url) => (url !== router.asPath) && setLoading(true);
+const handleComplete = (url) => (url === router.asPath) && setTimeout(() =>{setLoading(false)},5000);
+
+router.events.on('routeChangeStart', handleStart)
+router.events.on('routeChangeComplete', handleComplete)
+router.events.on('routeChangeError',  handleComplete)
+
+return () => {
+  router.events.off('routeChangeStart', handleStart)
+  router.events.off('routeChangeComplete', handleComplete)
+  router.events.off('routeChangeError', handleComplete)
+}
+})
   
-        router.events.on('routeChangeStart', handleStart)
-        router.events.on('routeChangeComplete', handleComplete)
-        router.events.on('routeChangeError',  handleComplete)
-  
-        return () => {
-            router.events.off('routeChangeStart', handleStart)
-            router.events.off('routeChangeComplete', handleComplete)
-            router.events.off('routeChangeError', handleComplete)
-        }
-    })
-    
-    return loading && (<div className='spinner-wrapper'>
-      <div className="spinner"> <Spin size="large"/></div></div>)
-  }
+return loading && (<div className='spinner-wrapper'><div className="spinner"> <Spin size="large"/></div></div>)
+}
   return (
     <div>
         <Loading />
       <BasketItem>
-        <ThemeProvider attribute="class">
-        
+        {/* <ThemeProvider attribute="class">  */}
           <Component {...pageProps} />
-        </ThemeProvider>
+        {/* </ThemeProvider> */}
       </BasketItem>
     </div>
   );
